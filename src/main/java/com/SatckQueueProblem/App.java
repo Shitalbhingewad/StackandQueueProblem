42ff76adca0fcba6f1ed15d7f278e9749869120c
shitalbhingewad <shitalbhingewad853@gmail.com>
2022-10-07 17:45:09 +0530

    Welcome to Stack and Queue Problem

---
+++ src/main/java/com/SatckQueueProblem/App.java
@@ -0,0 +1,11 @@
+package com.SatckQueueProblem;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+	private static final Logger logger = LogManager.getLogger(App.class);
+    public static void main( String[] args ) {
+       logger.info( "Welcome to Stack and Queue Problems" );
+      
+}
+}
